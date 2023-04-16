1b2e027ff13fb58db8dcbb3c956684347916f86c
Irina Smirnova <smirnovaiv20.02@gmail.com>
2023-04-16 11:45:19 +0300

    1

--- src/test/java/CurrentWeatherDataTest.java
+++ src/test/java/CurrentWeatherDataTest.java
@@ -1,7 +1,7 @@
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
 
-public class ApplicationTests {
+public class CurrentWeatherDataTest {
     @BeforeAll
     public static void setUp() {
         RestAssured.baseURI = "https://api.openweathermap.org/data/2.5";
